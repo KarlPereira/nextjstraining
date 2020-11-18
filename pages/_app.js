1d739cec539c1b2f27b3c9bc85c3304a56043fa4
KarlPereira <karlpereira14@gmail.com>
2020-11-18 23:38:04 +0100

    erreur_a_corriger_material_UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,42 @@
-import '../styles/globals.css'
+import React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider, responsiveFontSizes } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "constants/theme";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Ce composant _app nous permet de controler la façon dont l'application va s'initier.
+export default function MyApp(props) {
+	let ResponsiveFontTheme = responsiveFontSizes(theme);
+	const { Component, pageProps } = props;
+
+	React.useEffect(() => {
+		// Supprime le CSS injecté côté serveur.
+		const jssStyles = document.querySelector("#jss-server-side");
+		if (jssStyles) {
+			jssStyles.parentElement.removeChild(jssStyles);
+		}
+	}, []);
+
+	return (
+		<React.Fragment>
+			{/* La balise Head de Next s'utilise de la meme manière qu'un <head> en HTML5 */}
+			<Head>
+				<title>Next4Frogs</title>
+				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+			</Head>
+			{/* Initie la possibilité de modifié le theme global de notre application */}
+			<ThemeProvider theme={ResponsiveFontTheme}>
+				{/* CssBaseline est une base de référence CSS pour Material-UI */}
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</React.Fragment>
+	);
 }
 
-export default MyApp
+// Avec propTypes on s'assure que les props sont des objets requis et qu'un composant doit être présent.
+MyApp.propTypes = {
+	Component: PropTypes.elementType.isRequired,
+	pageProps: PropTypes.object.isRequired,
+};
